d7cbc05e407829290a68e5ef44178f671a9c1434
chipchocolate <patelmoulik5@gmail.com>
2014-08-12 19:09:11 -0400

    Create p44.py

---
+++ p44.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+def penta():
+    ps = set()
+    i = 0
+    while True:
+        i += 1
+        p = (3 * i * i - i) / 2
+        ps.add(p)
+        for n in ps:
+            if p - n in ps and p - 2 * n in ps:
+                return p - 2 * n
+
+print "D is:", penta()

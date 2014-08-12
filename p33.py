55b0d32d390f0799402f107444b19c633be8cf60
chipchocolate <patelmoulik5@gmail.com>
2014-08-12 19:04:54 -0400

    Create p33.py

---
+++ p33.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+d = 1
+
+for i in range(1, 10):
+    for j in range(1, i):
+        q, r = divmod(9 * j * i, 10 * j - i)
+        if not r and q <= 9:
+            d *= i / float(j)
+            print d
+
+print d
